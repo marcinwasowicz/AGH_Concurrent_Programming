29025f52fa470ffdfc25263b81027aef577637f1
marcinwasowicz <marcinwasowicz@LAPTOP-7TT223E8.localdomain>
2020-12-03 11:44:55 +0100

    parameters

--- src/ActiveObject/Parameters.java
+++ src/ActiveObject/Parameters.java
@@ -1,11 +1,11 @@
 package ActiveObject;
 
 public class Parameters {
-    public static final int numberOfProducers = 30;
-    public static final int numberOfConsumers = 30;
-    public static final int servantSize = 10;
-    public static final int maximumSize = 6;
-    public static final int additionalWork = 1000000;
+    public static final int numberOfProducers = 50;
+    public static final int numberOfConsumers = 50;
+    public static final int servantSize = 100;
+    public static final int maximumSize = 51;
+    public static final int additionalWork = 1000;
     public static final double task = 50000.0f;
-    public static final int batchSize = 10000;
+    public static final int batchSize = 50;
 }
--- src/Assignement1/Parameters.java
+++ src/Assignement1/Parameters.java
@@ -1,11 +1,11 @@
 package Assignement1;
 
 public class Parameters {
-    public static final int size = 10;
-    public static final int maxNumItems = 6;
-    public static final int numberProducers = 30;
-    public static final int numberConsumers = 30;
+    public static final int size = 100;
+    public static final int maxNumItems = 51;
+    public static final int numberProducers = 50;
+    public static final int numberConsumers = 50;
     public static final boolean hasWaitersMethod = false;
-    public static final int additionalWork = 1000000;
+    public static final int additionalWork = 1000;
     public static final double task = 50000.0f;
 }
