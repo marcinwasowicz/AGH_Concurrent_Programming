6614a0070bf3abb24b0cb4433b710d97af9cca77
marcinwasowicz <marcinwasowicz@LAPTOP-7TT223E8.localdomain>
2021-01-01 23:10:40 +0100

    experimenting with parameters

--- src/DistributedBufferCSP/Parameters.java
+++ src/DistributedBufferCSP/Parameters.java
@@ -1,8 +1,8 @@
 package DistributedBufferCSP;
 
 public class Parameters{
-    public static final int numberOfProducers = 5;
-    public static final int numberOfConsumers = 10;
-    public static final int[] layers = new int[] {1, 2, 3};
+    public static final int numberOfProducers = 30;
+    public static final int numberOfConsumers = 50;
+    public static final int[] layers = new int[] {1, 23, 1};
     public static final String messageConstant = "msg";
 }
