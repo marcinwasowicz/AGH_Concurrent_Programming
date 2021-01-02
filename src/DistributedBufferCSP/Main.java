c638363bacc3047dda03d6d873b03e7300c8a45d
marcinwasowicz <marcinwasowicz@LAPTOP-7TT223E8.localdomain>
2021-01-02 15:19:08 +0100

    parameters read from config.json, no need to recompile

--- src/DistributedBufferCSP/Main.java
+++ src/DistributedBufferCSP/Main.java
@@ -1,23 +1,28 @@
 package DistributedBufferCSP;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import org.jcsp.lang.Parallel;
+import org.json.simple.parser.ParseException;
 
-public class Main{
-    public static void main(String[] args) {
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
         ArrayList<Producer> producers = new ArrayList<>();
         ArrayList<Consumer> consumers = new ArrayList<>();
 
-        for(int i = 0; i<Parameters.numberOfProducers; i++){
-            producers.add(new Producer(Parameters.messageConstant + i));
+        Parameters parameters = new Parameters(args[0]);
+
+        for(int i = 0; i<parameters.getNumberOfProducers(); i++){
+            producers.add(new Producer(parameters.getMessageConstant() + i));
         }
 
-        for(int i = 0; i<Parameters.numberOfConsumers; i++){
+        for(int i = 0; i<parameters.getNumberOfConsumers(); i++){
            consumers.add(new Consumer());
         }
 
-        DistributedBuffer buffer = new DistributedBuffer(Parameters.layers);
+        DistributedBuffer buffer = new DistributedBuffer(parameters.getLayers());
 
         Parallel parallel = new Parallel(buffer.compile(producers, consumers));
         
--- src/DistributedBufferCSP/Parameters.java
+++ src/DistributedBufferCSP/Parameters.java
@@ -1,8 +1,38 @@
 package DistributedBufferCSP;
 
-public class Parameters{
-    public static final int numberOfProducers = 30;
-    public static final int numberOfConsumers = 50;
-    public static final int[] layers = new int[] {1, 23, 1};
-    public static final String messageConstant = "msg";
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class Parameters {
+    
+    private JSONObject config;
+
+    public Parameters(String configPath) throws FileNotFoundException, IOException, ParseException {
+        this.config = (JSONObject) new JSONParser().parse(new FileReader(configPath));
+    }
+
+    public int getNumberOfProducers(){
+        return (int) (long) this.config.get("numberOfProducers");
+    }
+
+    public int getNumberOfConsumers(){
+        return (int) (long) this.config.get("numberOfConsumers");
+    }
+
+    public String getMessageConstant(){
+        return (String) this.config.get("messageConstant");
+    }
+
+    public int[] getLayers(){
+       Object[] layers = ((JSONArray) this.config.get("layers")).toArray();
+       return Arrays.stream(layers).mapToInt(o -> (int)(long)o).toArray();
+    }
+
 }
